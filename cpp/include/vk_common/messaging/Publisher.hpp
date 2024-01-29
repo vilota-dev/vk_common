vilota-dev/vk_common

commit 285bc4897260c9c87ccde0fc389f38854b1cc03e

Reimplement messaging, removed TBB dependency

cpp/include/vk_common/messaging/Publisher.hpp
@@ -3,6 +3,10 @@
 
 #include "BroadcastQueue.hpp"
 #include "CallbackPool.hpp"
+#include <atomic>
+#include <chrono>
+#include <memory>
+#include <mutex>
 
 namespace vkc {
     /// Represents a publisher to a message type.
@@ -12,42 +16,55 @@ namespace vkc {
         explicit Publisher(
             SharedBroadcastQueue<T>& queue,
             SharedCallbackPool<T>& poolPre,
-            SharedCallbackPool<T>& poolPost,
-            const std::string_view topic
-        ): mQueue(queue), mPoolPre(poolPre), mPoolPost(poolPost), mTopic(topic) {}
+            SharedCallbackPool<T>& poolPost
+        ): mQueue(queue), mPoolPre(poolPre), mPoolPost(poolPost) {}
         Publisher(const Publisher&) = delete;
         Publisher(Publisher&&) = default;
         Publisher& operator=(Publisher&&) = default;
 
-        /// Send the given message to all subscribers.
+        /// Sends the given value to all subscribers.
+        ///
+        /// A `Message` is automatically created and wraps the given value, and other metadata
+        /// (such as the publish time etc.) are automatically populated.
         void send(T value) {
+            auto now = std::chrono::steady_clock::now();
+            auto duration = now.time_since_epoch();
+            this->send(Message<T> {
+                .mSequenceNumber = mQueue->mSequenceNumber.fetch_add(1, std::memory_order_relaxed),
+                .mPublishTime = static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::microseconds>(duration).count()),
+                .mPayload = std::move(value),
+            });
+        }
+
+        /// Sends the given message to all subscribers.
+        ///
+        /// The caller is in charge of constructing the `Message` instance and populating its metadata.
+        void send(Message<T> message) {
             {
-                std::scoped_lock<std::mutex> lock(this->mPoolPre->mMutex);
+                std::scoped_lock lock(mPoolPre->mMutex);
                 for (auto &f : this->mPoolPre->mPool) {
-                    f(value); // expect this may change the content
+                    f(message.mPayload); // expect this may change the content
                 }
             }
 
-            {
-                std::scoped_lock<std::mutex> lock(this->mQueue->mMutex);
-                for (auto &queue : this->mQueue->mQueues) {
-                    queue->push(value);
-                }
-            }
+            auto head = std::make_shared<typename BroadcastQueue<T>::Entry>();
 
-            {
-                std::scoped_lock<std::mutex> lock(this->mPoolPost->mMutex);
-                for (auto &f : this->mPoolPost->mPool) {
-                    f(value); // expect this may change the content
-                }
-            }
-
-            mCount++;
+#if __cplusplus >= 202002L
+            auto entry = mQueue->mHead.exchange(head, std::memory_order_relaxed);
+            entry->mMessage = std::move(message);
+            entry->mNext.store(head, std::memory_order_release);
+            entry->mNext.notify_all();
+#else
+            auto entry = std::atomic_exchange_explicit(&mQueue->mHead, head, std::memory_order_relaxed);
+            entry->mMessage = std::move(message);
+            std::atomic_store_explicit(&entry->mNext, head, std::memory_order_release);
+            entry->mCondVar.notify_all();
+#endif
         }
 
         /// Returns the topic name of this publisher.
         std::string_view topic() const {
-            return this->mTopic; 
+            return mQueue->topic();
         }
 
         /// Returns the number of messages this publisher has sent so far.
@@ -58,7 +75,6 @@ namespace vkc {
     private:
         SharedBroadcastQueue<T> mQueue;
         SharedCallbackPool<T> mPoolPre, mPoolPost;
-        std::string mTopic;
         uint64_t mCount = 0;
     };
 }

cpp/CMakeLists.txt
@@ -8,14 +8,6 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 # Dependencies.
 #======================================================
 
-find_package(TBB REQUIRED)
-message(STATUS "TBB FOUND ${TBB_VERSION_MAJOR}.${TBB_VERSION_MINOR} (interface version ${TBB_INTERFACE_VERSION}) headers in: ${TBB_INCLUDE_DIRS}")
-if (TBB_INTERFACE_VERSION LESS 11004)
-    # enable global_control header for earlier TBB versions (Ubuntu 16.04, 18.04)
-    add_definitions(-DTBB_PREVIEW_GLOBAL_CONTROL)
-endif()
-
-
 # static linking capnproto
 set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
 find_package(CapnProto REQUIRED)
@@ -95,4 +87,7 @@ target_include_directories(vk_common
         PRIVATE include/vk_common/capnp
         PUBLIC include)
 target_link_libraries(vk_common
-        PUBLIC CapnProto::capnp TBB::tbb)
+        PUBLIC CapnProto::capnp)
+
+add_executable(vk_test "tests/SimpleTest.cpp")
+target_link_libraries(vk_test PRIVATE vk_common)

cpp/include/vk_common/messaging/BroadcastQueue.hpp
@@ -1,15 +1,29 @@
 #ifndef VKC_BROADCASTQUEUE_HPP
 #define VKC_BROADCASTQUEUE_HPP
 
-#include <memory>
+#if __cplusplus < 202002L
 #include <mutex>
-#include <vector>
-#include <tbb/concurrent_queue.h>
+#include <condition_variable>
+#endif
+
+#include <atomic>
+#include <cstdint>
+#include <memory>
+#include <optional>
+#include <string_view>
 
 namespace vkc {
     template<typename T> class Publisher;
     template<typename T> class Subscriber;
     template<typename T> class BroadcastQueue;
+
+    /// Message containing a payload and its metadata.
+    template<typename T>
+    struct Message {
+        uint64_t mSequenceNumber;   //< Sequence number of message in its queue.
+        uint64_t mPublishTime;      //< Time instant, in miscroseconds, when the message is published.
+        T mPayload;                 //< Payload of the message (i.e. the actual data being sent).
+    };
 
     template<typename T>
     using SharedBroadcastQueue = std::shared_ptr<BroadcastQueue<T>>;
@@ -25,8 +39,17 @@ namespace vkc {
         BroadcastQueue(const BroadcastQueue<T>&) = delete;
         BroadcastQueue<T>& operator=(const BroadcastQueue<T>&) = delete;
 
-        static SharedBroadcastQueue<T> create() {
-            return std::shared_ptr<BroadcastQueue<T>>(new BroadcastQueue());
+        /// Return the topic of this queue.
+        std::string_view topic() const {
+            return mTopic;
+        }
+
+        static SharedBroadcastQueue<T> create(const std::string_view topic) {
+            auto queue = std::shared_ptr<BroadcastQueue<T>>(new BroadcastQueue());
+            queue->mTopic = topic;
+            queue->mSequenceNumber = 0;
+            queue->mHead = std::make_shared<Entry>();
+            return queue;
         }
 
     private:
@@ -35,8 +58,50 @@ namespace vkc {
         friend class Publisher<T>;
         friend class Subscriber<T>;
 
-        std::mutex mMutex;
-        std::vector<std::shared_ptr<tbb::concurrent_bounded_queue<T>>> mQueues;
+        class Entry {
+        public:
+            Entry() : mDummy(std::nullopt), mNext(nullptr) {}
+            ~Entry() {
+#if __cplusplus >= 202002L
+                auto next = mNext.load(std::memory_order_acquire);
+#else
+                auto next = std::atomic_load_explicit(&mNext, std::memory_order_acquire);
+#endif
+
+                if (next != nullptr) {
+                    mMessage.~Message<T>();
+                }
+            }
+
+        private:
+            friend class Publisher<T>;
+            friend class Subscriber<T>;
+
+            union {
+                Message<T> mMessage;    //< Message carried in this entry.
+                std::nullopt_t mDummy;  //< Value of the union when mMessage is uninitialized.
+            };
+
+#if __cplusplus >= 202002L
+            std::atomic<std::shared_ptr<Entry>> mNext;  //< Pointer to the next message in the queue.
+                                                        //< This must be accessed atomically.
+                                                        //< When this is `nullptr`, mMessage is uninitialized.
+                                                        //< This will only ever be written once.
+#else
+            std::mutex mMutex;                  //< Used in combination with condition variable to signal next message is ready.
+            std::condition_variable mCondVar;   //< Used in combination with mutex to signal next message is ready.
+            std::shared_ptr<Entry> mNext;       //< See the C++20 version above.
+#endif
+        }; // Entry
+
+        std::string mTopic;
+        std::atomic<uint64_t> mSequenceNumber;
+
+#if __cplusplus >= 202002L
+        std::atomic<std::shared_ptr<Entry>> mHead;
+#else
+        std::shared_ptr<Entry> mHead;
+#endif
     };
 }
 

cpp/include/vk_common/messaging/CallbackPool.hpp
@@ -7,7 +7,6 @@
 #include <functional>
 
 namespace vkc {
-
     template<typename T> class Publisher;
     template<typename T> class Subscriber;
     template<typename T> class CallbackPool;

cpp/include/vk_common/messaging/Registry.hpp
@@ -25,7 +25,7 @@ namespace vkc {
         template <typename T>
         Subscriber<T> getSubscriber(const std::string_view topic) {
             auto queue = getQueue<T>(topic);
-            return Subscriber<T>(queue, topic);
+            return Subscriber<T>(queue);
         }
 
         /// Constructs a publisher to a topic.
@@ -37,7 +37,7 @@ namespace vkc {
             auto queue = getQueue<T>(topic);
             auto poolPre = getCallbackPoolPre<T>(topic);
             auto poolPost = getCallbackPoolPost<T>(topic);
-            return Publisher<T>(queue, poolPre, poolPost, topic);
+            return Publisher<T>(queue, poolPre, poolPost);
         }
 
         /// Register an early callback for a topic.
@@ -79,7 +79,7 @@ namespace vkc {
         SharedBroadcastQueue<T> getQueue(const std::string_view topic) {
             auto it = mQueues.find(topic);
             if (it == mQueues.end()) {
-                it = mQueues.emplace(topic, BroadcastQueue<T>::create()).first;
+                it = mQueues.emplace(topic, BroadcastQueue<T>::create(topic)).first;
             }
 
             auto queue = std::dynamic_pointer_cast<BroadcastQueue<T>>(it->second);

cpp/include/vk_common/messaging/Subscriber.hpp
@@ -1,8 +1,10 @@
 #ifndef VKC_SUBSCRIBER_HPP
 #define VKC_SUBSCRIBER_HPP
 
+#include <atomic>
 #include <memory>
 #include <algorithm>
+#include <optional>
 #include "BroadcastQueue.hpp"
 #include "CallbackPool.hpp"
 
@@ -11,48 +13,17 @@ namespace vkc {
     template<typename T>
     class Subscriber {
     public:
-        explicit Subscriber(SharedBroadcastQueue<T>& parent, const std::string_view topic) : mParent(parent), mTopic(topic) {
-            auto queue = std::make_shared<tbb::concurrent_bounded_queue<T>>();
-            {
-                std::scoped_lock lock(mParent->mMutex);
-                mParent->mQueues.push_back(queue);
-            }
-            this->mQueue = queue;
-        };
-        ~Subscriber() {
-            if (mParent) {
-                std::scoped_lock lock(mParent->mMutex);
-                auto it = std::find(mParent->mQueues.begin(), mParent->mQueues.end(), this->mQueue);
-                if (it != mParent->mQueues.end()) {
-                    mParent->mQueues.erase(it);
-                }
-            }
-        }
+        Subscriber(SharedBroadcastQueue<T>& parent)
+            : mParent(parent),
+              mCurrent(mParent->mHead) {};
+        ~Subscriber() = default;
         Subscriber(const Subscriber&) = delete;
         Subscriber(Subscriber&&) = default;
         Subscriber& operator=(Subscriber&&) = default;
 
-        /// Block until there is an available message and then receives and returns it.
-        T recv() {
-            T result;
-            this->mQueue->pop(result);
-            this->mCount++;
-            return result;
-        }
-
-        /// Receives and writes the message at the given address if there is one immediately available, or
-        /// returns false otherwise.
-        bool tryRecv(T& result) {
-            if (this->mQueue->try_pop(result)) {
-                this->mCount++;
-                return true;
-            }
-            return false;
-        }
-
         /// Returns the topic name of this subscriber.
         std::string_view topic() const {
-            return this->mTopic;
+            return mParent->mTopic;
         }
 
         /// Returns the number of messages this subscriber has received so far.
@@ -60,10 +31,45 @@ namespace vkc {
             return mCount;
         }
 
+        /// Block until there is an available message and then receives and returns it.
+        Message<T> recv() {
+#if __cplusplus >= 202002L
+            mCurrent->mNext.wait(nullptr, std::memory_order_acquire);
+#else
+            {
+                std::unique_lock lock(mCurrent->mMutex);
+                while (std::atomic_load_explicit(&mCurrent->mNext, std::memory_order_acquire) == nullptr) {
+                    mCurrent->mCondVar.wait(lock);
+                }
+            }
+#endif
+            mCount++;
+            auto entry = mCurrent->mNext;
+            std::swap(mCurrent, entry);
+            return entry->mMessage;
+        }
+
+        /// Check if there is an available message and then receives and returns it.
+        /// If there is no available message, this method returns a `std::nullopt`.
+        std::optional<Message<T>> tryRecv() {
+#if __cplusplus >= 202002L
+            auto entry = mCurrent->mNext.load(std::memory_order_acquire);
+#else
+            auto entry = std::atomic_load_explicit(&mCurrent->mNext, std::memory_order_acquire);
+#endif
+
+            if (entry == nullptr) {
+                return std::nullopt;
+            } else {
+                mCount++;
+                std::swap(mCurrent, entry);
+                return entry->mMessage;
+            }
+        }
+
     private:
         SharedBroadcastQueue<T> mParent;
-        std::shared_ptr<tbb::concurrent_bounded_queue<T>> mQueue;
-        std::string mTopic;
+        std::shared_ptr<typename BroadcastQueue<T>::Entry> mCurrent;
         uint64_t mCount;
     };
 }

cpp/tests/SimpleTest.cpp
@@ -1,0 +1,22 @@
+#include <stdexcept>
+#include "vk_common/messaging/Registry.hpp"
+
+int main() {
+    vkc::Registry registry;
+    auto publisher = registry.getPublisher<int>("test_topic");
+    auto subscriber = registry.getSubscriber<int>("test_topic");
+    
+    publisher.send(123);
+    publisher.send(456);
+    auto message1 = subscriber.recv();
+    auto message2 = subscriber.tryRecv();
+
+    if (message1.mPayload != 123) {
+        throw std::runtime_error("Received value 1 does not match sent value!");
+    }
+    if (message2.value().mPayload != 456) {
+        throw std::runtime_error("Received value 2 does not match sent value!");
+    }
+
+    return 0;
+}

commit 75f44dac4999f72aea8f9da02726521790c9ddb6

fix mCount bug introduced in 285bc48

cpp/include/vk_common/messaging/Publisher.hpp
@@ -63,6 +63,8 @@ namespace vkc {
                     f(entry->mMessage.value());
                 }
             }
+
+            mCount++;
         }
 
         /// Returns the topic name of this publisher.